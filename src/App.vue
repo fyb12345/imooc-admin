a19377d17796585054aad3225d21653909ae7c0d
Roger.Feng <roger.feng@baimeihome.com>
2023-12-01 14:54:36 +0800

    feat: git 提交规规范

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+const app = '阿富汗的'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!"/>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <TheWelcome/>
   </main>
 </template>
 
